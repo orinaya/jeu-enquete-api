c7ae6778c000565e0a1694f4cc764b96f3f84ec4
Oriane FROUIN <oriane@floax.fr>
2024-11-27 21:52:29 +0100

    feat: add routes, app and hal

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const hostname = "localhost";
+const port = 3000;
+
+const charactersRouter = require("./routes/characters");
+const locationsRouter = require("./routes/locations");
+const itemsRouter = require("./routes/items");
+const cluesRouter = require("./routes/clues");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(charactersRouter, locationsRouter, itemsRouter, cluesRouter);
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
---
+++ routes/clues.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const { clues } = require("../database");
+const hal = require("../hal");
+
+router.get("/clues", (req, res, next) => {
+  res.status(200).json({
+    links: {
+      self: hal.halLinkObject("/clues"),
+    },
+    _embedded: clues,
+  });
+});
+
+router.get("/clues/:id(\\d+)", (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  const clue = clues.find((clue) => clue.clue_id === id);
+
+  if (clue === undefined) {
+    console.log("Le user n'a pas été trouvé");
+    return res.status(404).json({});
+  }
+
+  res.status(200).json({
+    _links: {
+      self: hal.halLinkObject(`/clues/${clue.clue_id}`),
+      clues: hal.halLinkObject(`/clues`),
+    },
+    clue_name: clue.clue_name,
+    clue_race: clue.clue_description,
+  });
+});
+
+module.exports = router;
